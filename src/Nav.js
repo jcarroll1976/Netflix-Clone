44ec4dd5e7382b84d9884da3f6a5e49525638bc6
Joshua Carroll <carrolljosh1976@gmail.com>
2022-11-01 23:00:25 -0400

    Started Netflix clone

---
+++ src/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Nav.css"
+
+function Nav() {
+  return (
+    <div className="nav">
+        <div className="nav_contents">
+        <img className = "nav_logo" src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt=""/>
+        <img className = "nav_avatar" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117" alt=""/>
+        </div>
+        
+        <h1>This is the nav.</h1>
+    </div>
+  )
+}
+
+export default Nav
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD4UbglYVuydlGvxabctTGbZz2M9pIKTXs",
+    authDomain: "netflix-clone-dd0e3.firebaseapp.com",
+    projectId: "netflix-clone-dd0e3",
+    storageBucket: "netflix-clone-dd0e3.appspot.com",
+    messagingSenderId: "869580113998",
+    appId: "1:869580113998:web:d972f535b60ccb89f59b1f"
+  };
