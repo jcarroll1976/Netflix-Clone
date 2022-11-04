e86213831796d7cc87a35ed1537bddd79ece2f5d
Joshua Carroll <carrolljosh1976@gmail.com>
2022-11-03 21:32:02 -0400

    Added components and got sone data from API

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,7 +1,9 @@
-import React from 'react'
-import "./HomeScreen.css"
-import Nav from './Nav'
-import Banner from './Banner'
+import React from 'react';
+import "./HomeScreen.css";
+import Nav from "./components/Nav";
+import Banner from './components/Banner';
+import requests from './services/Requests';
+import Row from './components/Row';
 
 function HomeScreen() {
   return (
@@ -10,10 +12,22 @@ function HomeScreen() {
     <Nav />
     
     <Banner />
-        {/*Banner*/}
 
-        {/*Row*/}
+    <Row
+    title = "NETFLIX ORIGINALS"
+    fetchURL = {requests.fetchNetflixOriginals}
+    isLargeRow />
 
+    <Row title = "Trending Now" fetchURL = {requests.fetchTrending} />
+    <Row title = "Top Rated" fetchURL = {requests.fetchTopRated} />
+    <Row title = "Action Movies" fetchURL = {requests.fetchActionMovies} />
+    <Row title = "Comedy Movies" fetchURL = {requests.fetchComedyMovies} />
+    <Row title = "Horror Movies" fetchURL = {requests.fetchHorrorMovies} />
+    <Row title = "Romance Movies" fetchURL = {requests.fetchRomanceMovies} />
+    <Row title = "Documentaries" fetchURL = {requests.fetchDocumentaries} />
+    
+    
+    
     </div>
   )
 }
--- src/services/Requests.js
+++ src/services/Requests.js
@@ -1,12 +1,14 @@
-const API_KEY = "1237e9ec60c0dc6c3f58b3a39512982a";
+const API_KEY = "b2a5b6426fb561221a39b0fed3c6b168"
 
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-    fetchTopRated: `/trending/all/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
     fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
     fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
     fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
     fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
-}
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`
+};
+
+export default requests;
